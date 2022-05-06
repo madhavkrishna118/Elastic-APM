86416c22cf41feb6ba19e745e54c478f7ab71dd7
Maddy <kattamadhav63@gmail.com>
2022-05-06 13:39:30 +0530

    apm

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen('3050',function(req,res) {
   console.log('express server started at port 3050');
 })
+
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+
+  // Override the service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: 'Testing APM',
+  
+  // Use if APM Server requires a secret token
+  secretToken: 'QzdP4BfL4RhrQLw9jj',
+  
+  // Set the custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'http://localhost:8200',
+  
+  // Set the service environment
+  environment: 'production'
+  })
